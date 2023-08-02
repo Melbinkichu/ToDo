35f6e503dec78af2370c929d879db58fff85de36
Melbin K P <122599371+Melbinkichu@users.noreply.github.com>
2023-08-02 15:28:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function handlesubmit(callback){
+    var id=document.getElementById('username').value;
+    var pass=document.getElementById('password').value;
+    console.log(id);
+    console.log(pass);
+    if( id ==="admin"&& pass ==="12345"){
+        callback()
+    }else{
+        alert("invalid");
+    }
+
+
+}
+function validate(){
+    window.location.href="todo.html"
+}
